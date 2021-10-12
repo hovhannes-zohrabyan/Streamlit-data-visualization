88efc368ee68a715bd34fec5bc66289037c746cf
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2021-10-12 17:21:50 +0400

    Added Checkboxes

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ st.title('Test Application')
 
 st.write("Here's our first attempt at using data to create a table:")
 
-st.title('See Dataframe')
-df = pd.read_csv("Data/breast-cancer-winsconsin.csv")
+st.title('First 20 Rows of housing dataset')
+df = pd.read_csv("Data/housing.csv")
 st.write(df.head(20))
 
-st.title('Radius and texture plots')
-st.line_chart(df[['radius_mean', 'texture_mean']])
+st.title('Median Income and House Value Plots')
+st.line_chart(df[['median_income', 'median_house_value']])
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
 
 # progress_bar = st.sidebar.progress(0)
 # status_text = st.sidebar.empty()
