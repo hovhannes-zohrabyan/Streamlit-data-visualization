411f31a4cc285043b6d878612fd9cd16500efd3c
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2021-10-12 17:18:36 +0400

    Added small line charts

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ st.title('See Dataframe')
 df = pd.read_csv("Data/breast-cancer-winsconsin.csv")
 st.write(df.head(20))
 
+st.title('Radius and texture plots')
+st.line_chart(df[['radius_mean', 'texture_mean']])
 
 # progress_bar = st.sidebar.progress(0)
 # status_text = st.sidebar.empty()
