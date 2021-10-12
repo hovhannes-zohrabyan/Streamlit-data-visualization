49a6e87459df3b176f86c6d934f16eed9a7d868c
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2021-10-12 17:17:23 +0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import time
+import numpy as np
+import pandas as pd
+
+st.title('Test Application')
+
+
+
+# progress_bar = st.sidebar.progress(0)
+# status_text = st.sidebar.empty()
+
+st.button("Re-run")
