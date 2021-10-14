e1612175dd8a67f7c8f69a87e18f77b3e6ccba32
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2021-10-14 21:37:02 +0400

    * Small Updates on Style * Added Sidebar * Second Chart Updated to histogram * Columned Graph Added

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import streamlit as st
-import time
+import seaborn as sns
 import numpy as np
 import pandas as pd
 
-st.title('Test Application')
-
-st.write("Here's our first attempt at using data to create a table:")
-
-st.title('First 20 Rows of housing dataset')
+# Read Data
 df = pd.read_csv("Data/housing.csv")
+
+# Add Sidebar to the application
+sidebar_selectbox_value = st.sidebar.selectbox(
+    'Please Choose column to plot in the second graph',
+    df.columns
+)
+
+st.title('Example service created to showcase some of the features of the application')
+
+st.write("We will use a housing dataset used in book Hands On Machine Learning with Scikit Learn and Tensorflow for "
+         "regression tasks")
+
+st.subheader('First 20 rows of the dataset')
 st.write(df.head(20))
 
-st.title('Median Income and House Value Plots')
-st.line_chart(df[['median_income']])
+st.subheader('Median Income and House Value Histograms')
+histogram_data, bins = np.histogram(df['median_income'], bins=45)
+st.bar_chart(histogram_data)
 
 st.title('Map Plot of points from dataset')
 map_data = df[['latitude', 'longitude']]
@@ -42,6 +52,24 @@ st.latex(r'''
 st.title("Slider Control Example")
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
+
+
+st.title("Widgets can also be placed Side-by-Side for better view")
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+
+# Or even better, call Streamlit functions inside a "with" block:
+with left_column:
+    chosen = st.radio(
+        'Choose Ocean Proximity Here', np.unique(df['ocean_proximity'])
+    )
+
+with right_column:
+    len = len(df[df['ocean_proximity'] == chosen]['median_income'])
+    st.write("Number of Houses in this range is : %s" % (len))
+    map_data = df[df['ocean_proximity'] == chosen][['latitude', 'longitude']]
+    st.map(map_data)
+
 
 
 st.button("Restart the page")
